d7cdf79db86b8dcca1dcb69e63acc6d57745c73c
정재용 <wodydthk7@hanyang.ac.kr>
2021-06-10 20:45:26 +0900

    item-based CF recommender system df to numpy

--- long_term_project/recommender.py
+++ long_term_project/recommender.py
@@ -5,6 +5,12 @@ from sklearn.metrics.pairwise import cosine_similarity
 
 
 def get_sparse_matrix(user_item_list):    
+    # ---------- 
+    # user-item-rating list를 sparse matrix로 변환해준다.
+    #   user_item_list : user-item-rating을 row로 갖는 dataframe
+    #   return : user-item을 행, 열로 갖고 rating을 나타내는 sparse matrix
+    # ----------
+
     _list_columns = user_item_list.columns
     _index, _columns, _value = _list_columns
 
@@ -14,27 +20,48 @@ def get_sparse_matrix(user_item_list):
     
 
 def cal_item_similarity(user_item_rating):
-    # cosine similarity
-    sim_array = cosine_similarity(user_item_rating.T)
-
-    # item - item similarity
-    item_index = user_item_rating.columns
-    return pd.DataFrame(sim_array, index=item_index, columns=item_index)
+    # ---------- 
+    # item 간의 코사인 유사도를 구한다.
+    #   user_item_rating : user-item-rating을 나타내는 sparse matrix
+    #   return : item 간의 코사인 유사도를 계산한 array
+    # ----------
     
+    return cosine_similarity(user_item_rating.T)
 
-def expect_user_item_rating(user_item_rating, sim_df, user_id, item_id):
+def get_user_rating_avg(user_item_rating, user_id):
+    # ---------- 
+    # user가 item들에 대해 매긴 평점의 평균을 구한다.    
+    #   user_item_rating : user-item-rating을 나타내는 sparse matrix
+    #   user_id : 구하고자 하는 유저의 id
+    #   return : 유저가 매긴 평점들의 평균
+    # ----------
+
     user_rating = user_item_rating.loc[user_id]
-    item_corr = sim_df[item_id]
-    
+    user_rating_drop = user_rating[user_rating > 0]
 
-    rating_corr = pd.DataFrame([user_rating, item_corr], index=['rating', 'correlation']).T
-    top_rating = rating_corr[rating_corr['rating'] > 0].sort_values('correlation', ascending=False).head(10)
-    
-    values = top_rating['rating'].values
-    weight = top_rating['correlation'].values
-    
-    return np.dot(values, weight) / sum(weight)
+    return int(round(np.average(user_rating_drop.values), 0))
 
+
+def expect_user_item_rating(user_item_rating, sim_array, user_id, item_id):
+    # ---------- 
+    # user가 item에 대해 매긴 평점을 예측한다.
+    #   user_item_rating : user-item-rating을 나타내는 sparse matrix
+    #   sim_array : 아이템 간의 코사인 유사도
+    #   user_id : 구하고자 하는 유저의 id
+    #   item_id : 예측하고자 하는 아이템의 id
+    #   return : 아이템에 대한 평점의 예측값 (정수)
+    # ----------
+
+    # 유저가 아이템들에 매긴 평점과, 구하려는 아이템과 다른 아이템들간의 유사도를 가져온다.
+    user_rating = user_item_rating.loc[user_id]
+    item_index = np.where(user_item_rating.columns == item_id)[0][0]
+    item_corr = sim_array[item_index]
+
+    rating_corr =  np.array([user_rating, item_corr]).T
+    rating_corr_drop = rating_corr[rating_corr[:, 0] > 0] # 유저가 평점을 매기지 않은 아이템들은 지운다.
+    top_rating = rating_corr_drop[np.argsort(rating_corr_drop, axis=0).T[1]][::-1][0:13, :].T # 유사도 상위 13개 아이템을 가져온다.
+    
+    return int(round(np.average(top_rating[0],weights=top_rating[1]), 0))
 
 
 if __name__ == '__main__':
@@ -46,18 +73,24 @@ if __name__ == '__main__':
     test_file = pd.read_csv('./data/u1.test', sep='\t', names=columns)
 
     user_item_list = base_file.drop('time_stamp', axis=1)    
+    test_rating = test_file['rating'].values
     test_user_item_list = test_file.drop(['time_stamp', 'rating'], axis=1)
 
     user_item_rating = get_sparse_matrix(user_item_list)
-    sim_df = cal_item_similarity(user_item_rating)
+    sim_array = cal_item_similarity(user_item_rating)
 
 
     result = []
+    # 각 user-item 쌍에 대해 예측값을 구한다.
+    # 기존 데이터에 없던 item에 대해서는 유저가 매긴 평점들의 평균으로 예측한다.
     for idx, user_id, item_id in test_user_item_list.itertuples():
         if item_id in user_item_rating.columns:
-            result.append(expect_user_item_rating(user_id, item_id))
+            result.append(expect_user_item_rating(user_item_rating, sim_array, user_id, item_id))
         else:
-            result.append(0)
+            result.append(get_user_rating_avg(user_item_rating, user_id))
 
+    
+    
+    
     test_user_item_list['rating'] = result
     test_user_item_list.to_csv(f'{base_file_name}_prediction.txt', sep='\t', index=False, header=False)
